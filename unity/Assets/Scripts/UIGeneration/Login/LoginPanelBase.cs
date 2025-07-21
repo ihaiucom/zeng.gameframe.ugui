e98a0d73fef92d40d364d3e4c41004050151348f
zengfeng75 <zengfeng75@qq.com>
2025-07-21 19:21:29 +0800

    CodeBindExtend, UIComponent

--- unity/Assets/Scripts/UIGeneration/Login/LoginPanelBase.cs
+++ unity/Assets/Scripts/UIGeneration/Login/LoginPanelBase.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 using UnityEngine.UI;
 using System.Collections.Generic;
 using Cysharp.Threading.Tasks;
+using Sirenix.OdinInspector;
 
 namespace Games.UI.Login
 {
@@ -16,18 +17,30 @@ namespace Games.UI.Login
     /// </summary>
     public abstract class LoginPanelBase:UIPanel
     {
+        [ShowInInspector]
         public const string PkgName = "Login";
+        [ShowInInspector]
         public const string ResName = "LoginPanel";
         
+        [ShowInInspector]
         public override EWindowOption WindowOption => EWindowOption.None;
+        [ShowInInspector]
         public override EPanelLayer Layer => EPanelLayer.Panel;
+        [ShowInInspector]
         public override EPanelOption PanelOption => EPanelOption.None;
+        [ShowInInspector]
         public override EPanelStackOption StackOption => EPanelStackOption.VisibleTween;
+        [ShowInInspector]
         public override int Priority => 0;
+        [ShowInInspector]
         public TMPro.TMP_InputField u_ComUserNameInput { get; private set; }
+        [ShowInInspector]
         public UnityEngine.RectTransform u_ComPasswordInput { get; private set; }
+        [ShowInInspector]
         public UnityEngine.UI.Toggle u_ComRememberLogin { get; private set; }
+        [ShowInInspector]
         public UnityEngine.UI.Button u_ComReigisterButton { get; private set; }
+        [ShowInInspector]
         public UnityEngine.UI.Button u_ComLoginButton { get; private set; }
 
         
