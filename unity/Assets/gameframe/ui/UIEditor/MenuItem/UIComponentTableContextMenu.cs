b6c47035b012a66864c931fc3e0b046be015a256
zengfeng75 <zengfeng75@qq.com>
2025-07-24 22:33:25 +0800

    UIComponentTableContextMenu  [MenuItem("CONTEXT/Component/添加到UI组件表", false, -int.MaxValue)]

---
+++ unity/Assets/gameframe/ui/UIEditor/MenuItem/UIComponentTableContextMenu.cs
@@ -0,0 +1,30 @@
+﻿#if UNITY_EDITOR
+using UnityEditor;
+using UnityEngine;
+
+namespace Zeng.GameFrame.UIS.Editor
+{
+    public class UIComponentTableContextMenu
+    {
+        [MenuItem("CONTEXT/Component/添加到UI组件表", false, -int.MaxValue)]
+        private static void AddUIComponentTable(MenuCommand command)
+        {
+            if (!UIOperationHelper.CheckUIOperation()) return;
+            if (command.context is not Component menuComponent) return;
+            var cdeTable = Selection.activeGameObject.GetComponentInParent<UIBindCDETable>();
+            if (cdeTable == null) return;
+            cdeTable.ComponentTable ??= cdeTable.gameObject.AddComponent<UIBindComponentTable>();
+            cdeTable.ComponentTable.EditorAddComponent(menuComponent);
+        }
+
+        [MenuItem("CONTEXT/Component/添加到UI组件表", true)]
+        private static bool AddUIComponentTableValidate(MenuCommand command)
+        {
+            if (Selection.activeGameObject == null) return false;
+            var cdeTable = Selection.activeGameObject.GetComponentInParent<UIBindCDETable>();
+            if (cdeTable == null) return false;
+            return true;
+        }
+    }
+}
+#endif
