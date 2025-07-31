231e7c97e45eae44023c068e3953010a7c2d8b68
zengfeng75 <zengfeng75@qq.com>
2025-07-31 10:40:14 +0800

    Test

--- unity/Assets/Scripts/UIGeneration/I2Localization/I2Localize.cs
+++ unity/Assets/Scripts/UIGeneration/I2Localization/I2Localize.cs
@@ -8,16 +8,6 @@ namespace I2.Loc
 		public static string LangIcon 		{ get{ return LocalizationManager.GetTranslation ("LangIcon"); } }
 		public static string LanguageIcon 		{ get{ return LocalizationManager.GetTranslation ("LanguageIcon"); } }
 		public static string Test 		{ get{ return LocalizationManager.GetTranslation ("Test"); } }
-
-		public static class Sprite
-		{
-			public static string GameIcon 		{ get{ return LocalizationManager.GetTranslation ("Sprite/GameIcon"); } }
-		}
-
-		public static class Text
-		{
-			public static string GameName 		{ get{ return LocalizationManager.GetTranslation ("Text/GameName"); } }
-		}
 	}
 
     public static class I2Terms
@@ -26,15 +16,5 @@ namespace I2.Loc
 		public const string LangIcon = "LangIcon";
 		public const string LanguageIcon = "LanguageIcon";
 		public const string Test = "Test";
-
-		public static class Sprite
-		{
-		    public const string GameIcon = "Sprite/GameIcon";
-		}
-
-		public static class Text
-		{
-		    public const string GameName = "Text/GameName";
-		}
 	}
 }
